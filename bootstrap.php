c4258f3413a8ade73dafeaddef874a0a989d68a4
Ncrediblebulk <ajrock622@gmail.com>
2018-01-05 17:47:52 -0500

    inital commint

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = require "Config.php";
+
+require "functions.php";
+
+require "database/Connection.php";
+
+require "database/QueryBuilder.php";
+
+return new QueryBuilder(Connection::make($config["database"]));
